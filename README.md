d89ea213d0f4be46ac1bf95deaab2d1c521acb8d
danie1kr <github.daniel@kauker.com>
2020-05-09 23:55:48 +0200

    fully working with artwork and covers, plug detect and external amp

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # musikschlumpf
+
+# errata
+Feather board miso/mosi pin are switched :/
+Never use pin13 for something which should not be high on boot :/
+you might want to change the SD usage in Adafruit_vs1053 as attached in Schlumpf_Adafruit_VS1053, see startPlayingFile
--- musikschlumpf.ino
+++ musikschlumpf.ino
@@ -10,7 +10,7 @@
 #include <Adafruit_ImageReader.h> // Image-reading functions
 #include <Chrono.h>
 #include <Bounce2.h>
-#include <Adafruit_VS1053.h>
+#include "Schlumpf_Adafruit_VS1053.h"
 #include <MFRC522.h>
 #include <Wire.h>
 #include "Adafruit_TPA2016.h"
@@ -31,6 +31,10 @@ const char DELIMITER = ';';
 const char* SHUFFLE_FILE = "_shuffle";
 const char* COVER_FILE = "_cover.bmp";
 const char* ACTIONS_FILE = "musikschlumpf.txt";
+const char* WALLPAPER_DIR = "wallpape";
+const char* FULL_WALLPAPER_DIR = "/wallpape";
+
+const int SHUTDOWN_TIMEOUT = 20*60;
 
 const int PIN_SDCARD_CS = 10;
 const int PIN_VS1053_CS = 11;
@@ -42,9 +46,9 @@ const int PIN_OLED_CS = 7;
 const int PIN_OLED_DC = 9;
 const int PIN_OLED_RST = -1;
 
-const int PIN_MFRC522_CS = 3;
-const int PIN_MFRC522_RST = 4;
-const int PIN_MFRC522_IRQ = 0;
+#define PIN_MFRC522_CS 3
+#define PIN_MFRC522_RST 4// UINT8_MAX //;//4;
+//const int PIN_MFRC522_IRQ = 0;
 
 const int PIN_BUTTON_PLAY = A3;
 const int PIN_BUTTON_NEXT = A2;
@@ -55,7 +59,7 @@ const int PIN_AMP_SD = 2;
 
 const int PIN_PLUG_DETECT = A5;
 
-const int PIN_SHUTDOWN = 13;
+const int PIN_SHUTDOWN = 0;//4; // never 13!
 
 const int PIN_BATTERY_PROBE = A0;
 
@@ -63,7 +67,7 @@ const int DISPLAY_ROTATION = 0;
 
 #define RGB565(RGB88) (((RGB88&0xf80000)>>8) + ((RGB88&0xfc00)>>5) + ((RGB88&0xf8)>>3))
 const uint16_t COLOR_BLACK = RGB565(0x0);
-const uint16_t COLOR_WHITE = RGB565(0x0);
+const uint16_t COLOR_WHITE = RGB565(0xFFFFFF);
 const uint16_t COLOR_BACKGROUND = COLOR_BLACK;
 const uint16_t COLOR_TEXT = COLOR_WHITE;
 
@@ -89,7 +93,7 @@ const unsigned int SCREEN_HEIGHT = 128;
 Adafruit_SSD1351 display = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, PIN_OLED_CS, PIN_OLED_DC, PIN_OLED_RST);
 
 // Music
-Adafruit_VS1053_FilePlayer musicPlayer = Adafruit_VS1053_FilePlayer(PIN_VS1053_RST, PIN_VS1053_CS, PIN_VS1053_DCS, PIN_VS1053_DREQ, PIN_SDCARD_CS);
+Schlumpf_Adafruit_VS1053_FilePlayer musicPlayer = Schlumpf_Adafruit_VS1053_FilePlayer(PIN_VS1053_RST, PIN_VS1053_CS, PIN_VS1053_DCS, PIN_VS1053_DREQ, PIN_SDCARD_CS);
 
 // AMP
 Adafruit_TPA2016 audioamp = Adafruit_TPA2016();
@@ -126,6 +130,36 @@ typedef struct
 
 std::vector<Action> actions;
 
+/// File listing helper
+void printDirectory(File dir, int numTabs) {
+   while(true) {
+     
+     File entry =  dir.openNextFile();
+     if (! entry) {
+       // no more files
+       //Serial.println("**nomorefiles**");
+       break;
+     }
+     for (uint8_t i=0; i<numTabs; i++) {
+       Serial.print('\t');
+     }
+     char n[128];
+     size_t l = 128;
+     entry.getName(n, l);
+     Serial.print(n);
+     //Serial.print(entry.name());
+     if (entry.isDirectory()) {
+       Serial.println("/");
+       printDirectory(entry, numTabs+1);
+     } else {
+       // files have sizes, directories do not
+       Serial.print("\t\t");
+       Serial.println(entry.size(), DEC);
+     }
+     entry.close();
+   }
+}
+
 std::string dirname(std::string file)
 {
 	size_t found = file.find_last_of("/\\");
@@ -149,7 +183,11 @@ int isBatteryGood(bool ignoreTimer = false)
 
 	if (ignoreTimer || chronoCheckBattery.hasPassed(5000, true))
 	{
-		if (analogRead(PIN_BATTERY_PROBE) < 400)
+		int voltage = analogRead(PIN_BATTERY_PROBE);
+		DEBUG_PRINT_VAR("voltage read", voltage);
+		DEBUG_PRINT_VAR("voltage V", (voltage + 1) * 0.0169f);
+
+		if (voltage < 400)
 			return 0;
 	}
 	return 1;
@@ -173,7 +211,8 @@ void display_init()
 void display_hello()
 {
 	display_clear();
-	display_text("Musikschlumpf ist bereit", 4, 48, 3, COLOR_WHITE);
+	//display_text("Musikschlumpf bereit", 4, 48, 1, COLOR_WHITE);
+	wallpaper();
 }
 
 void display_clear()
@@ -183,9 +222,29 @@ void display_clear()
 
 void display_status()
 {
-	display.fillRect(0,0, 127, 10, COLOR_BLACK);
-	if(playing)
-		display_text("play", 9, 2, 2, COLOR_WHITE);
+	//display.fillRect(0,0, 127, 10, COLOR_BLACK);
+	//if(playing)
+	//	display_text("play", 9, 2, 2, COLOR_WHITE);
+}
+
+/**
+ * Helper routine to dump a byte array as hex values to Serial. 
+ */
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
+
+/**
+ * Helper routine to dump a byte array as dec values to Serial.
+ */
+void printDec(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 10 ? " 0" : " ");
+    Serial.print(buffer[i], DEC);
+  }
 }
 
 void setupDisplay()
@@ -209,6 +268,10 @@ void setupSD()
 	}
 
 	sdRoot = SD.open("/");
+	currentDirectory = SD.open("/");
+	#ifdef DEBUG
+	printDirectory(sdRoot, 0);
+	#endif
 
 	DEBUG_PRINTLN("done");
 }
@@ -252,18 +315,23 @@ void setupRFID()
 {
 	DEBUG_PRINT("setup RFID... ");
 
-	SPI.begin(); // Init SPI bus
+	//SPI.begin(); // Init SPI bus
 	rfid.PCD_Init(); // Init MFRC522 
+	/*rfid.PCD_DumpVersionToSerial();
+	if (rfid.PCD_PerformSelfTest())
+	{
+		//DEBUG_PRINTLN("Passed Self-Test");
+	}*/
 
 	for (byte i = 0; i < 6; i++) {
 		rfidKey.keyByte[i] = 0xFF;
 	}
 
-	Serial.println(F("This code scan the MIFARE Classsic NUID."));
-	Serial.print(F("Using the following key:"));
-	printHex(rfidKey.keyByte, MFRC522::MF_KEY_SIZE);
+	//Serial.println(F("This code scan the MIFARE Classsic NUID."));
+	//Serial.print(F("Using the following key:"));
+	//printHex(rfidKey.keyByte, MFRC522::MF_KEY_SIZE);
 
-	DEBUG_PRINTLN("done");
+	DEBUG_PRINTLN(" done");
 }
 
 void setupRandomizer()
@@ -279,41 +347,16 @@ void setupButtonsAndVolume()
 	pinMode(PIN_PLUG_DETECT, INPUT_PULLUP);
 
 	buttonPlay.attach(PIN_BUTTON_PLAY);
-	buttonPlay.interval(25);
+	buttonPlay.interval(16);
 	buttonPrev.attach(PIN_BUTTON_PREV);
-	buttonPrev.interval(25);
+	buttonPrev.interval(16);
 	buttonNext.attach(PIN_BUTTON_NEXT);
-	buttonNext.interval(25);
+	buttonNext.interval(16);
 	plugDetect.attach(PIN_PLUG_DETECT);
-	plugDetect.interval(25);
+	plugDetect.interval(16);
 
 	// PIN_POT_VOLUME
 	analogReadResolution(10);
-}
-
-void setup()
-{
-	pinMode(PIN_SHUTDOWN, OUTPUT);
-	digitalWrite(PIN_SHUTDOWN, LOW);
-
-	Serial.begin(115200);
-	while (!Serial) { delay(1); }
-
-	DEBUG_PRINTLN("setup musikschlumpf... ");
-
-	setupDisplay();
-	setupAMP();
-	setupSD();
-	setupActions();
-	setupMusic();
-	setupButtonsAndVolume();
-	setupRFID();
-
-	setupRandomizer();
-
-	display_hello();
-
-	DEBUG_PRINTLN("setup musikschlumpf... done");
 }
 
 void checkButtons()
@@ -323,7 +366,7 @@ void checkButtons()
 	buttonPrev.update();
 	buttonNext.update();
 
-	if(buttonPlay.read() == LOW)
+	if(buttonPlay.fell())
 	{
 		DEBUG_PRINT("button Play: ");
 		if (! musicPlayer.paused()) {
@@ -337,13 +380,13 @@ void checkButtons()
 
 		action = true;
 	}
-	if(buttonNext.read() == LOW)
+	if(buttonNext.fell())
 	{
 		DEBUG_PRINTLN("button Next");
 		playNext();
 		action = true;
 	}
-	if(buttonPrev.read() == LOW)
+	if(buttonPrev.fell())
 	{
 		DEBUG_PRINTLN("button Prev");
 		musicPlayer.stopPlaying();
@@ -359,12 +402,23 @@ void checkButtons()
 
 void play(std::string file)
 {
-	if(musicPlayer.isMP3File(file.c_str()))
+	if(1)//musicPlayer.isMP3File(file.c_str()))
 	{
-		musicPlayer.startPlayingFile(file.c_str());
+
+		DEBUG_PRINT_VAR("files test", file.c_str());
+		SD.chdir("/", true);
+		//if(SD.open(file.c_str())) { DEBUG_PRINTLN(" works"); } else {  DEBUG_PRINTLN(" failed"); }
+
+		if(musicPlayer.startPlayingFile(SD, file.c_str()))
+		{
+			DEBUG_PRINT_VAR("playing file", file.c_str());
+		}
+		else
+		{
+			DEBUG_PRINT_VAR("playing file failed", file.c_str());
+		}
 		currentMP3 = file;
 
-		DEBUG_PRINT_VAR("playing file: ", file.c_str());
 	}
 	else
 	{
@@ -374,12 +428,16 @@ void play(std::string file)
 
 std::string getRandomFile()
 {
+	if(playlist.size() == 0)
+		return std::string("");
 	currentPlaylistIndex = random(0, playlist.size()-1);
 	return playlist[currentPlaylistIndex];
 }
 
 std::string getNextFile()
 {
+	if(playlist.size() == 0)
+		return std::string("");
 	currentPlaylistIndex = (currentPlaylistIndex + 1) % playlist.size();
 	return playlist[currentPlaylistIndex];
 }
@@ -392,7 +450,8 @@ void playNext()
 	else
 		mp3 = getNextFile();
 
-	play(mp3);
+	if(!mp3.empty())
+		play(mp3);
 }
 
 bool isShuffleDir(File directory)
@@ -413,22 +472,33 @@ void generatePlaylist(std::string fullDirectoryPath, File directory)
 	playlist.clear();
 	currentPlaylistIndex = 0;
 
+	size_t NAME_LEN = 128;
+	char name[NAME_LEN];
+	directory.getName(name, NAME_LEN);
+	DEBUG_PRINT_VAR("scanning playlist in", name);
+	#ifdef DEBUG
+	printDirectory(directory, 0);
+	#endif
+	directory.rewind();
+	DEBUG_PRINT_VAR("full dir path is", fullDirectoryPath.c_str());
+
 	File entry;
 	while(entry = directory.openNextFile())
 	{
+		entry.getName(name, NAME_LEN);
+
+		DEBUG_PRINT_VAR("checking file", name);
 		if(!entry.isFile())
 			continue;
 
-		size_t NAME_LEN = 13;
-		char name[NAME_LEN];
-		entry.getName(name, NAME_LEN);
 		if(name[0] == '_')
 			continue;
 
 		std::string fullFilePath = "/";
 		fullFilePath.append(fullDirectoryPath).append("/").append(name);
 
-		if(musicPlayer.isMP3File(fullFilePath.c_str()))
+		DEBUG_PRINT_VAR("full path is", fullFilePath.c_str());
+		//if(musicPlayer.isMP3File(fullFilePath.c_str()))
 			playlist.push_back(fullFilePath);
 	}
 
@@ -445,14 +515,27 @@ void playByNewCard()
 	{
 		if(compare(action.card, nuidPICC))
 		{
+			SD.chdir("/", true);
 			currentPathDirectory = action.file;
 			if(sdRoot.exists(currentPathDirectory.c_str()))
 			{
+				if(!musicPlayer.paused())
+					musicPlayer.stopPlaying();
+				DEBUG_PRINT_VAR("selected directory", currentPathDirectory.c_str());
 				currentDirectory.close();
-				sdRoot.open(&currentDirectory, currentPathDirectory.c_str(), O_RDONLY);
+				#ifdef DEBUG
+				currentDirectory.open(&sdRoot, currentPathDirectory.c_str(), O_RDONLY);
+				printDirectory(currentDirectory, 0);
+				#endif
+				currentDirectory.open(&sdRoot, currentPathDirectory.c_str(), O_RDONLY);
 				generatePlaylist(currentPathDirectory, currentDirectory);
 				displayCover(currentPathDirectory, currentDirectory);
 				playNext();
+				chronoShutDown.restart(0);
+			}
+			else
+			{
+				DEBUG_PRINT_VAR("does not exist in /", currentPathDirectory.c_str());
 			}
 			return;
 		}
@@ -467,6 +550,7 @@ void playByNewCard()
 
 void displayCover(std::string fullDirectoryPath, File directory)
 {
+	display_clear(); delay(100);
 	if(directory.exists(COVER_FILE))
 	{
 		currentCover = "/";
@@ -483,20 +567,36 @@ void displayCover(std::string fullDirectoryPath, File directory)
 		}
 	}
 	else
-		display_text(fullDirectoryPath.c_str(), 3, 32);
+		display_text(fullDirectoryPath.c_str(), 3, 96);
+}
+
+void wallpaper()
+{
+	display_clear(); delay(100);
+	File wallpaperDir = SD.open(FULL_WALLPAPER_DIR);
+	generatePlaylist("wallpape", wallpaperDir);
+	std::string wallpaperPath = getRandomFile();
+
+	playlist.clear();
+	currentPlaylistIndex = 0;
+
+	{
+		char *wallpaper_cstr = new char[wallpaperPath.length() + 1];
+		strcpy(wallpaper_cstr, wallpaperPath.c_str());
+		ImageReturnCode stat = reader.drawBMP(wallpaper_cstr, display, 0, 0);
+		delete[] wallpaper_cstr;
+
+		if(stat != IMAGE_SUCCESS)
+		{
+			DEBUG_PRINT_VAR("failed to display wallpaper", wallpaperPath.c_str());
+			DEBUG_PRINT_VAR("status", stat);
+		}
+	}
 }
 
 void checkHeadphonePlugAndVolume()
 {
 	plugDetect.update();
-	int v = analogRead(PIN_POT_VOLUME) / 4;
-
-	if(abs(v-volume) > 5)
-	{
-		volume = v;
-		musicPlayer.setVolume(volume, volume);
-		DEBUG_PRINT_VAR("new volume", volume);
-	}
 
 	bool plugDetected = plugDetect.read() == HIGH;
 	if(plugDetected != headphonePluggedIn)
@@ -516,93 +616,100 @@ void checkHeadphonePlugAndVolume()
 			DEBUG_PRINTLN("headphone removed");
 		}
 	}
-}
 
-void loop()
-{
-	checkButtons();
-	checkHeadphonePlugAndVolume();
+	int v = analogRead(PIN_POT_VOLUME) / 4;
 
-	isBatteryGood();
-
-	if(checkRFIDForNewCard())
+	if(abs(v-volume) > 5)
 	{
-		playByNewCard();
+		volume = v;
+		int setVol = map(volume, 0, 255, 42, 255);
+		musicPlayer.setVolume(volume, volume);
+		DEBUG_PRINT_VAR("new volume", volume);
 	}
-
-	if(chronoShutDown.hasPassed(2*60*60))
-	{
-		DEBUG_PRINTLN("goodbye");
-		digitalWrite(PIN_SHUTDOWN, HIGH);
-	}
-
-	delay(500);
 }
 
 bool checkRFIDForNewCard()
 {
-	  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
-  if ( ! rfid.PICC_IsNewCardPresent())
-    return false;
+	// Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+	if ( ! rfid.PICC_IsNewCardPresent())
+	{
+		//DEBUG_PRINTLN("no new card present");
+		return false;
+	}
 
-  // Verify if the NUID has been readed
-  if ( ! rfid.PICC_ReadCardSerial())
-    return false;
+	// Verify if the NUID has been readed
+	if ( ! rfid.PICC_ReadCardSerial())
+	{
+		//DEBUG_PRINTLN("NUID not read");
+		return false;
+	}
 
-  Serial.print(F("PICC type: "));
-  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
-  Serial.println(rfid.PICC_GetTypeName(piccType));
+	Serial.print(F("PICC type: "));
+	MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+	Serial.println(rfid.PICC_GetTypeName(piccType));
 
-  // Check is the PICC of Classic MIFARE type
-  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
-    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
-    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
-    Serial.println(F("Your tag is not of type MIFARE Classic."));
-    return false;
-  }
+	// Check is the PICC of Classic MIFARE type
+	if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+		piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+		piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+		DEBUG_PRINTLN(F("Your tag is not of type MIFARE Classic."));
+		return false;
+	}
 
-  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
-    rfid.uid.uidByte[1] != nuidPICC[1] || 
-    rfid.uid.uidByte[2] != nuidPICC[2] || 
-    rfid.uid.uidByte[3] != nuidPICC[3] ) {
-    Serial.println(F("A new card has been detected."));
+	if (rfid.uid.uidByte[0] != nuidPICC[0] || 
+	rfid.uid.uidByte[1] != nuidPICC[1] || 
+	rfid.uid.uidByte[2] != nuidPICC[2] || 
+	rfid.uid.uidByte[3] != nuidPICC[3] ) {
+		DEBUG_PRINTLN(F("A new card has been detected."));
 
-    // Store NUID into nuidPICC array
-    for (byte i = 0; i < 4; i++) {
-      nuidPICC[i] = rfid.uid.uidByte[i];
-    }
-   
-    Serial.println(F("The NUID tag is:"));
-    Serial.print(F("In hex: "));
-    printHex(rfid.uid.uidByte, rfid.uid.size);
-    Serial.println();
-    Serial.print(F("In dec: "));
-    printDec(rfid.uid.uidByte, rfid.uid.size);
-    Serial.println();
-  }
-  else Serial.println(F("Card read previously."));
+		// Store NUID into nuidPICC array
+		for (byte i = 0; i < 4; i++) {
+			nuidPICC[i] = rfid.uid.uidByte[i];
+		}
 
-  // Halt PICC
-  rfid.PICC_HaltA();
+		Serial.println(F("The NUID tag is:"));
+		Serial.print(F("In hex: "));
+		printHex(rfid.uid.uidByte, rfid.uid.size);
+		Serial.println();
+		Serial.print(F("In dec: "));
+		printDec(rfid.uid.uidByte, rfid.uid.size);
+		Serial.println();
+	}
+	else 
+		DEBUG_PRINTLN(F("Card read previously."));
 
-  // Stop encryption on PCD
-  rfid.PCD_StopCrypto1();
+	// Halt PICC
+	rfid.PICC_HaltA();
 
-  return true;
+	// Stop encryption on PCD
+	rfid.PCD_StopCrypto1();
+
+	return true;
 }
 
 byte charToByte(char c)
 {
+	byte b = '0';
+
 	if(c >= '0' && c <= '9')
-		return (c - '0');
+		b = (c - '0');
 	else if (c >= 'A' && c <= 'F') 
-		return (10 + (c - 'A'));
+		b = (10 + (c - 'A'));
 	else if (c >= 'a' && c <= 'f')
-		return (10 + (c - 'a'));
+		b = (10 + (c - 'a'));
+	else
+	{
+	#ifdef DEBUG
+		b = c;
+		DEBUG_PRINT("charToByte unkown char: hex(");
+		printHex(&b, 1);
+		DEBUG_PRINT(") | dec(");
+		printDec(&b, 1);
+		DEBUG_PRINTLN(")");
+	#endif
+	}
 
-	DEBUG_PRINT_VAR("charToByte unkown char", c);
-
-	return 0;
+	return b;
 }
 
 void setupActions()
@@ -646,11 +753,11 @@ void setupActions()
 	    		else if(c == DELIMITER)
 	    		{
 	    			for(unsigned int i = 0; i < 4; ++i)
-	    				action.card[i] = (charToByte(buffer[2*i+0]) << 4) + (charToByte(buffer[2*i+0]));
+	    				action.card[i] = (charToByte(buffer[2*i+0]) << 4) + (charToByte(buffer[2*i+1]));
 	    			READ++;
+	    			index = 0;
 	    		}
-
-	    		if(index < BUFFER_SIZE)
+	    		else if(index < BUFFER_SIZE)
 	    		{
 	    			buffer[index] = c;
 	    			++index;
@@ -662,32 +769,71 @@ void setupActions()
 
 #ifdef DEBUG
     Serial.println("I parsed the following actions:");
-	for(const auto& action: actions) {
-	    Serial.printf("  %c:%c:%c:%c : %s\n", action.card[0], action.card[1], action.card[2], action.card[3], action.file.c_str());
+	for(auto& action: actions) {
+		printHex(action.card, 4);
+	    Serial.print(":");
+		Serial.println(action.file.c_str());
 	}
 	Serial.println("");
 #endif
 }
 
-/**
- * Helper routine to dump a byte array as hex values to Serial. 
- */
-void printHex(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], HEX);
-  }
+void setup()
+{
+	pinMode(PIN_SHUTDOWN, OUTPUT);
+	digitalWrite(PIN_SHUTDOWN, HIGH);
+
+	pinMode(PIN_SDCARD_CS, OUTPUT);
+	digitalWrite(PIN_SDCARD_CS, HIGH);
+	pinMode(PIN_VS1053_CS, OUTPUT);
+	digitalWrite(PIN_VS1053_CS, HIGH);
+	pinMode(PIN_OLED_CS, OUTPUT);
+	digitalWrite(PIN_OLED_CS, HIGH);
+	pinMode(PIN_MFRC522_CS, OUTPUT);
+	digitalWrite(PIN_MFRC522_CS, HIGH);
+
+	SPI.begin(); // Init SPI bus
+	Serial.begin(9600);
+	#ifdef DEBUG
+	delay(2000);
+	#endif
+	//while (!Serial) { delay(1); }
+	DEBUG_PRINTLN("setup musikschlumpf... ");
+
+	setupMusic();
+	setupSD();
+	setupActions();
+	setupDisplay();
+	setupAMP();
+	setupButtonsAndVolume();
+	setupRFID();
+
+	setupRandomizer();
+
+	display_hello();
+
+	DEBUG_PRINTLN("setup musikschlumpf... done");
 }
 
-/**
- * Helper routine to dump a byte array as dec values to Serial.
- */
-void printDec(byte *buffer, byte bufferSize) {
-  for (byte i = 0; i < bufferSize; i++) {
-    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
-    Serial.print(buffer[i], DEC);
-  }
+void loop()
+{
+	checkButtons();
+	checkHeadphonePlugAndVolume();
+
+	isBatteryGood();
+
+	if(checkRFIDForNewCard())
+		playByNewCard();
+
+	if(chronoShutDown.hasPassed(SHUTDOWN_TIMEOUT))
+	{
+		DEBUG_PRINTLN("goodbye");
+		digitalWrite(PIN_SHUTDOWN, HIGH);
+	}
+
+	delay(25);
 }
+
 
 /*
  #include <SD.h>
